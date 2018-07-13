251e0d117cee44544ad2b9352d3ab666b7748ddb
Christie Hennes <christiehennes@gmail.com>
2018-07-13 13:35:31 -0700

    Updated coloring, added contact form

--- main.js
+++ main.js
@@ -5,11 +5,12 @@ let portfolioItem = [];
 //Create an object for a portfolio item 
 
 //Function to initalize all the portfolio objects and add them to an array 
-function addPortfolioItem(name, ghCode, ghProject){
+function addPortfolioItem(name, type, ghCode, ghProject){
     console.log("here");
 
     let item = {
         Name: name,
+        Type: type,
         GitHubCode: ghCode,
         GitHubProject: ghProject
     }
@@ -22,12 +23,19 @@ function displayPortfolio(){
 
     portfolioItem.forEach(function(item){
 
+        // <img class="portfolio-item-image" src="./images/crystalcollector.png">
+
         let portfolioDiv = 
         `<div class="portfolio-item">
+            
             <div class="info-wrapper">
                 <div class="project-name">${item.Name}</div>
-                <div class="code-link">${item.GitHubCode}</div>
-                <div class="project-link">${item.GitHubLink}</div>
+                <div class="project-type">${item.Type}</div>
+                <div class="code-link">
+                     <a href=${item.GitHubCode}><i class="fas fa-code"></i></a>
+
+                </div>
+                <div class="project-link">${item.GitHubProject}</div>
             </div>
         </div>
         `
@@ -37,10 +45,34 @@ function displayPortfolio(){
 }
 
 
+$("a").on('click', function(event) {
 
-addPortfolioItem("Project 1", "link", "link");
-addPortfolioItem("Project 2", "link", "link");
-addPortfolioItem("Project 3", "link", "link");
-addPortfolioItem("Project 4", "link", "link");
-addPortfolioItem("Project 5", "link", "link");
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+
+
+
+addPortfolioItem("Music Venue Search", "Multiple APIs + js", "https://github.com/christiehennes/Project1", "lhttps://christiehennes.github.io/Project1/");
+addPortfolioItem("Giphy", "API", "https://github.com/christiehennes/GIPHY-Project", "https://christiehennes.github.io/GIPHY-Project/");
+addPortfolioItem("Train Scheduler", "firebase", "https://github.com/christiehennes/Train-Scheduler", "https://christiehennes.github.io/Train-Scheduler/");
+addPortfolioItem("Word Guess", "js", "https://github.com/christiehennes/Word-Guess-Game", "https://christiehennes.github.io/Word-Guess-Game/");
+addPortfolioItem("Crystal Collector", "js", "https://github.com/christiehennes/unit-4-game", "https://christiehennes.github.io/unit-4-game/");
+addPortfolioItem("NYC Trivia", "js", "https://github.com/christiehennes/TriviaGame", "https://christiehennes.github.io/TriviaGame/");
 displayPortfolio();
